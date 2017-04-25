5135bd34733f6fe5a7a8f690c622c83194495deb
Thomas Hein <thomas@onlineth.com>
2017-04-24 22:58:10 -0500

    Update every minute & update if image path changes

--- SpotlightWallpaper.py
+++ SpotlightWallpaper.py
@@ -1,10 +1,9 @@
-import schedule
 import winreg
 import ctypes
 import sys
 import time
 
-# Simple message box function
+# Simple message box function 
 def Mbox(title, text, style):
     ctypes.windll.user32.MessageBoxW(0, text, title, style)
 
@@ -14,7 +13,6 @@ def getCurrentSpotlightImage():
         hKey = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
                               "SOFTWARE\Microsoft\Windows\CurrentVersion\Lock Screen\Creative")
         asset_path = winreg.QueryValueEx(hKey, "LandscapeAssetPath")
-        print('Current Spotlight Image found')
     except:
         schedule.CancelJob()
         Mbox("SpotlightDesktop | ERROR", 'Cannot find the registry information of the Spotlight image location.', 0)
@@ -37,16 +35,21 @@ def ChangeWallpaper(image_location):
 # The function that ties everything together
 def SpotlightDesktop():
     SpotlightImage = getCurrentSpotlightImage()
+    print('Spotlight image location found: '+SpotlightImage)
     ChangeWallpaper(SpotlightImage)
     print('Finished updating the current Spotlight image')
+    return SpotlightImage
+
+# Function that checks if the Spotlight image changes
+def CheckForChange(image_location):
+    if (image_location != getCurrentSpotlightImage()):
+        print('Spotlight image changed, changing background')
+        SpotlightDesktop()
 
 # Run it once now
-SpotlightDesktop()
-
-# Schedule SpotlightDesktop to run every day
-schedule.every().hour.do(SpotlightDesktop)
+image_location = SpotlightDesktop()
 
 # Let it run in the background
 while True:
-    schedule.run_pending()
+    CheckForChange(image_location)
     time.sleep(60)
