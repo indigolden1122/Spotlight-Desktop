a8588a6ed359fc87ee335e64378864c7a1d09081
Thomas Hein <thomas@onlineth.com>
2017-10-23 19:24:28 -0500

    Use a precompiled RegLookup executable
    This is able to lookup the registery info where looking it up in the script (or even a non-compiled binary) would not be able to access.

--- SpotlightDesktop.py
+++ SpotlightDesktop.py
@@ -4,6 +4,7 @@ import sys
 import time
 import winreg
 import subprocess
+import os
 
 
 def mbox(title, text, style):
@@ -15,44 +16,74 @@ def getcurrentdpotlightimage():
     """Gets the current Spotlight image path"""
 
     # Why run a command? Try getting that through the winreg library... (permissions issues)
-    try:
-        output = subprocess.check_output("reg query HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative", shell=True, stderr=subprocess.STDOUT).decode('utf8').splitlines()
-    except subprocess.CalledProcessError:
-        location = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Lock Screen\\Creative"
-
+    while(1): 
         try:
-            hkey = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
-                                  location)
-            asset_path = winreg.QueryValueEx(hkey, "LandscapeAssetPath")
-        except FileNotFoundError:
-            mbox("spotlightdesktop | ERROR", 'Cannot find the registry information' +
-                 'of the Spotlight image location.', 0)
-            sys.exit()
+            location = subprocess.check_output("RegLookup.exe HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative", shell=True, stderr=subprocess.STDOUT).decode('utf8').splitlines()[-1]
 
-        # Return only the value from the resulting tuple (value, type_as_int).
-        return asset_path[0]
+            # Sadely, not the real location
+            try:
+                location = subprocess.check_output("RegLookup.exe "+location, shell=True, stderr=subprocess.STDOUT).decode('utf8').splitlines()[-1]
 
-    for x in output:
-        if(x != ""):
-            location = x
+                # And yes, even when we find the location, that's protected too
+                try:
+                    
+                    output = subprocess.check_output("RegLookup.exe " + location + " /v landscapeImage", shell=True, stderr=subprocess.STDOUT).decode('utf8')
+                except subprocess.CalledProcessError:
+                    print("Breaked from Level 3")
+                    break
 
-    # And yes, even when we find the location, that's protected too
+                # Get whatever is after the last space (that should be the image path)
+                location = output[output.rindex(' ')+1:]
+
+                if (location == ""):
+                    mbox("spotlightdesktop | ERROR", 'Found the registry information' +
+                         'but it did not include a path to the Spotlight image location.', 0)
+                    sys.exit()
+
+                return location
+
+            except subprocess.CalledProcessError:
+                print("Breaked from Level 2")
+                break
+
+        except subprocess.CalledProcessError:
+            print("Breaked from Level 1")
+            break
+
+    print("Using old registry location")
+
+    location = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Lock Screen\\Creative"
+
     try:
-        output = subprocess.check_output("reg query " + location + " /v LandscapeAssetPath", shell=True, stderr=subprocess.STDOUT).decode('utf8')
-    except subprocess.CalledProcessError:
-        mbox("spotlightdesktop | ERROR", 'Cannot find the registry information based on the new path' +
+        hkey = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
+                              location)
+        asset_path = winreg.QueryValueEx(hkey, "LandscapeAssetPath")
+    except FileNotFoundError:
+        mbox("spotlightdesktop | ERROR", 'Cannot find the registry information' +
              'of the Spotlight image location.', 0)
         sys.exit()
 
-    location = output[output.rindex(' ')+1:] 
-    return location
+    # Check to make sure there is content
+    if (asset_path[0] == ""):
+        mbox("spotlightdesktop | ERROR", 'Found the registry information' +
+             'but it did not include a path to the Spotlight image location.', 0)
+        sys.exit()
+
+    # Return only the value from the resulting tuple (value, type_as_int).
+    return asset_path[0]
 
 
 def changewallpaper(location):
     """Changes the Windows Wallpaper"""
+    SPIF_UPDATEINIFILE  = 0x01
+    SPIF_SENDCHANGE     = 0x02
+    SPI_SETDESKWALLPAPER    = 0x0014
+    WM_SETTINGCHANGE    = 0x1A
+    HWND_BROADCAST      = 0xFFFF
+    SPI_GETDESKWALLPAPER    = 0x0073
+
     try:
-        spi_setdeskwallpaper = 20
-        ctypes.windll.user32.SystemParametersInfoW(spi_setdeskwallpaper, 0, location, 3)
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 1, location, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE)
         print('Desktop wallpaper changed')
     except FileNotFoundError:
         mbox("spotlightdesktop | ERROR", 'Cannot change the desktop wallpaper to' +
