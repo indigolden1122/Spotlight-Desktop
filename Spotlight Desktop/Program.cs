07eca2abde914f9384161f6daa649fb0a0790732
Thomas Hein <thomas@onlineth.com>
2018-03-27 01:23:07 -0500

    AttachConsole() instead of Debug.
    Debug.WriteLine() only is used when debugging and not on release. This will attach a console and allow for Console.Witeline().

--- Spotlight Desktop/Program.cs
+++ Spotlight Desktop/Program.cs
@@ -4,15 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 
 namespace Spotlight_Desktop
 {
     class Program
     {
+        [DllImport("kernel32.dll")]
+        private static extern void AttachConsole(int dwProcessId);
+
         static void Main(string[] args)
         {
-            Debug.WriteLine("Hello World");
+            AttachConsole(-1);
+            Console.WriteLine("Hello World");
         }
     }
 }
