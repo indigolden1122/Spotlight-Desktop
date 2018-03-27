6d8d2a47587dac02b4b3986fc1abb05f8984c36f
Thomas Hein <thomas@onlineth.com>
2018-03-27 17:40:14 -0500

    Add RegLookupParse Class
    No need for RegLookup.exe, thanks to @anthonyslas for finding this,
                RegistryKey localKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
                localKey = localKey.OpenSubKey(keyPath, false);
    
    Which seems to bring up the intended values and doesn't run into permissions issues as other typical registry functions like Registry.GetValue() does.
    
    This can find and list all the direct values of a key and list the sub keys as well. With the dive parameter set to true, it can also recursively go through those sub keys and and make it's own RegLookupParse object on it and then put them all listed into the SubKeys.SubkeyObj.
    
    Co-Authored-By: Anthony Slas <anthonyslas@users.noreply.github.com>

---
+++ Spotlight Desktop/RegLookupParse.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Microsoft.Win32;
+
+namespace Spotlight_Desktop
+{
+    public class RegLookupParse
+    {
+        public struct KeyValue
+        {
+            public string Name;
+            public string Value;
+        }
+
+        public struct SubKey
+        {
+            public string KeyPath;
+            public RegLookupParse SubKeyObj;
+        }
+
+
+        public bool Error = false;
+        public List<KeyValue> KeyValues = new List<KeyValue>();
+        public List<SubKey> SubKeys = new List<SubKey>();
+
+
+        // keyPath (path to key), dive (recursevly go through SubKeys)
+        public RegLookupParse(string keyPath, bool dive = true)
+        {
+            RegistryKey localKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            localKey = localKey.OpenSubKey(keyPath, false);
+
+            // Check for empty (non-exsistent) key
+            if (localKey is null)
+            {
+                Error = true;
+                return;
+            }
+
+            // Process any Sub Keys
+            foreach (string subKeyName in localKey.GetSubKeyNames())
+            {
+                string subKeyPath = keyPath + @"\" + subKeyName;
+
+                // Check if it's needed to go through the subkeys
+                if (dive)
+                {
+                    SubKeys.Add(new SubKey()
+                    {
+                        KeyPath = subKeyPath,
+                        SubKeyObj = new RegLookupParse(subKeyPath)
+                    });
+                }
+                else
+                {
+                    SubKeys.Add(new SubKey()
+                    {
+                        KeyPath = subKeyPath
+                    });
+                }
+            }
+
+            // Process any Values
+            foreach (string valueName in localKey.GetValueNames())
+            {
+                string valueForName = localKey.GetValue(valueName).ToString();
+                KeyValues.Add(new KeyValue() { Name = valueName, Value = valueForName });
+            }
+        }
+    }
+}
