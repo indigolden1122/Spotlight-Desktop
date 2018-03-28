c1cf8ef090aca1455fe0088ff6faf596471c128b
Thomas Hein <thomas@onlineth.com>
2018-03-27 23:13:54 -0500

    Formatting Adjustments
    No changes to the code

--- Spotlight Desktop/FindImage.cs
+++ Spotlight Desktop/FindImage.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Spotlight_Desktop
 {
-    static class FindImage
+    internal static class FindImage
     {
-        private const string CreativePath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative";
-
+        private const string CreativePath =
+            @"SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative";
         private const string OldCreativePath =
             @"SOFTWARE\Microsoft\Windows\CurrentVersion\Lock Screen\Creative";
         private static readonly string UserLocalAppData =
@@ -25,53 +26,36 @@ namespace Spotlight_Desktop
 
             // *Currently used on the latest Windows 10 Release*
             // This goes through [Phase 1] SubKeys, then the last [Phase 2] SubKey
-            foreach (RegLookupParse.SubKey currentPhase1Key in creative.SubKeys)
-            {
+            foreach (var currentPhase1Key in creative.SubKeys)
                 if (currentPhase1Key.SubKeyObj.SubKeys.Count != 0)
-                {
-                    // Okay, there is a [Phase 2] Sub Key, find the last one and go through it's Values in search of "landscapeImage"
-                    foreach (RegLookupParse.KeyValue phase2KeyValue in currentPhase1Key.SubKeyObj.SubKeys.Last().SubKeyObj.KeyValues)
-                    {
+                    foreach (var phase2KeyValue in currentPhase1Key.SubKeyObj.SubKeys.Last().SubKeyObj.KeyValues)
                         if (phase2KeyValue.Name == "landscapeImage")
                         {
                             // Check to make sure it belongs to this account
                             if (phase2KeyValue.Value.StartsWith(UserLocalAppData)) return phase2KeyValue.Value;
                             break;
                         }
-                    }
-                }
-            }
 
             // This goes through [Phase 1] SubKeys, and searches it's Key Values for "LandscapeAssetPath"
             // (Used for an older Windows 10 update where [Phase 2] didn't exsist)
             // (This does not go into [Phase 2])
-            foreach (RegLookupParse.SubKey currentPhase1Key in creative.SubKeys)
-            {
-                // We have a [Phase 2] SubKey, now find the "landscapeImage" in the KeyValues
-                foreach (RegLookupParse.KeyValue phase2KeyValue in currentPhase1Key.SubKeyObj.KeyValues)
+            foreach (var currentPhase1Key in creative.SubKeys)
+            // We have a [Phase 2] SubKey, now find the "landscapeImage" in the KeyValues
+            foreach (var phase2KeyValue in currentPhase1Key.SubKeyObj.KeyValues)
+                if (phase2KeyValue.Name == "LandscapeAssetPath")
                 {
-                    if (phase2KeyValue.Name == "LandscapeAssetPath")
-                    {
-                        // Check to make sure it belongs to this account
-                        if (phase2KeyValue.Value.StartsWith(UserLocalAppData)) return phase2KeyValue.Value;
-                        break;
-                    }
+                    // Check to make sure it belongs to this account
+                    if (phase2KeyValue.Value.StartsWith(UserLocalAppData)) return phase2KeyValue.Value;
+                    break;
                 }
-            }
 
             // Should occur if on an old Windows Update
             var oldCreative = new RegLookupParse(OldCreativePath, false);
 
             if (!oldCreative.Error)
-            {
-                foreach (RegLookupParse.KeyValue keyName in oldCreative.KeyValues)
-                {
+                foreach (var keyName in oldCreative.KeyValues)
                     if (keyName.Name == "LandscapeAssetPath")
-                    {
                         if (keyName.Value.StartsWith(UserLocalAppData)) return keyName.Value;
-                    }
-                }
-            }
 
             return null;
         }
